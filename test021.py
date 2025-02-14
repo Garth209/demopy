aa679297264e247f6366ad7232cc9715f89c5472
Garth209 <garthprimms618@gmail.com>
2025-02-14 15:45:48 +0900

    Create test021.py
    带参数的decorator

---
+++ test021.py
@@ -0,0 +1,14 @@
+"""
+带参数的decorator
+"""
+
+import functools
+
+def log(text):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
